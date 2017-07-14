73b5bdac515cd76936b277a6a31cf4f3b5819015
Jay Connor <mjzeel@gmail.com>
2017-07-14 12:49:46 +0530

    files updated with new dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
