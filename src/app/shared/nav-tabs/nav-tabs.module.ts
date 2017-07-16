5f001a806cb23b4997d7a6e98bf1da9942d3c2ce
suneth wijayasiri <suneth wijayasiri>
2017-07-16 22:00:55 +0530

    add asset module

--- src/app/shared/nav-tabs/nav-tabs.module.ts
+++ src/app/shared/nav-tabs/nav-tabs.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavTabsComponent } from './nav-tabs.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		RouterModule
+		RouterModule,
+		MaterialModule
 	],
 	declarations: [
 		NavTabsComponent
