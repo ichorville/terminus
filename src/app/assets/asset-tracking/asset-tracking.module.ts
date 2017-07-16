8d59a418e496c28212fff239550d8c3b81c94303
suneth wijayasiri <suneth wijayasiri>
2017-07-16 22:00:55 +0530

    add asset module

---
+++ src/app/assets/asset-tracking/asset-tracking.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssetTrackingComponent } from './asset-tracking.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AssetTrackingComponent]
+})
+export class AssetTrackingModule { }
--- src/app/shared/pagination/pagination.module.ts
+++ src/app/shared/pagination/pagination.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './pagination.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		MaterialModule
 	],
 	declarations: [
 		PaginationComponent
