b8d3a121b8abc5411aa5ac0128bf68c3c04c74ec
Jay <mjzeel@gmail.com>
2017-08-19 19:11:40 +0530

    geography routing changed to use child routes

--- src/app/master-data/geography-master/district-master/district-master.routing.ts
+++ src/app/master-data/geography-master/district-master/district-master.routing.ts
@@ -1,28 +1,40 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SidenavComponent } from '../../../sidenav/sidenav.component';
 import { DistrictListComponent } from './district-list/district-list.component';
 import { DistrictAddComponent } from './district-add/district-add.component';
 import { DistrictEditComponent } from './district-edit/district-edit.component';
 
 const districtMasterRoutes: Routes = [
 	{
-		path: '',
-		redirectTo: '/master-data/geographies/districts',
-		pathMatch: 'full'
-	},
-	{
-		path: 'master-data/geographies/districts',
-		component: DistrictListComponent
-	},
-	{
-		path: 'master-data/geographies/districts/add',
-		component: DistrictAddComponent
-	},
-	{
-		path: 'master-data/geographies/districts/:id/edit',
-		component: DistrictEditComponent
-	}
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: SidenavComponent,
+        children: [
+            {
+				path: '',
+				redirectTo: '/master-data/geographies/districts',
+				pathMatch: 'full'
+			},
+			{
+				path: 'master-data/geographies/districts',
+				component: DistrictListComponent
+			},
+			{
+				path: 'master-data/geographies/districts/add',
+				component: DistrictAddComponent
+			},
+			{
+				path: 'master-data/geographies/districts/:id/edit',
+				component: DistrictEditComponent
+			}
+        ]
+    }
 ]
 
 export const routing: ModuleWithProviders = RouterModule.forChild(districtMasterRoutes);
--- src/app/master-data/geography-master/town-master/town-master.routing.ts
+++ src/app/master-data/geography-master/town-master/town-master.routing.ts
@@ -1,28 +1,40 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SidenavComponent } from '../../../sidenav/sidenav.component'; 
 import { TownListComponent } from './town-list/town-list.component';
 import { TownAddComponent } from './town-add/town-add.component';
 import { TownEditComponent } from './town-edit/town-edit.component';
 
 const townMasterRoutes: Routes = [
 	{
-		path: '',
-		redirectTo: '/master-data/geographies/towns',
-		pathMatch: 'full'
-	},
-	{
-		path: 'master-data/geographies/towns',
-		component: TownListComponent
-	},
-	{
-		path: 'master-data/geographies/towns/add',
-		component: TownAddComponent
-	},
-	{
-		path: 'master-data/geographies/towns/:id/edit',
-		component: TownEditComponent
-	}
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: SidenavComponent,
+        children: [
+            {
+				path: '',
+				redirectTo: '/master-data/geographies/towns',
+				pathMatch: 'full'
+			},
+			{
+				path: 'master-data/geographies/towns',
+				component: TownListComponent
+			},
+			{
+				path: 'master-data/geographies/towns/add',
+				component: TownAddComponent
+			},
+			{
+				path: 'master-data/geographies/towns/:id/edit',
+				component: TownEditComponent
+			}
+        ]
+    }
 ]
 
 export const routing: ModuleWithProviders = RouterModule.forChild(townMasterRoutes);
