13e76336d40f42ca340881e3231b38474673a174
Jay <mjzeel@gmail.com>
2017-08-19 14:05:55 +0530

    home routing updated

--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -1,17 +1,24 @@
 import { ModuleWithProviders } 	from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SidenavComponent } from '../sidenav/sidenav.component';
 import { HomeComponent } from './home.component';
 
 const homeRoutes: Routes = [
     {
-		path:'',
-		redirectTo: '/home',
+		path: '',
+		redirectTo: '/login',
 		pathMatch: 'full'
 	},
 	{
-		path: 'home',
-		component: HomeComponent
+		path: '',
+		component: SidenavComponent,
+		children: [
+			{
+				path: 'home',
+				component: HomeComponent
+			}
+		]
 	}
 ];
 
