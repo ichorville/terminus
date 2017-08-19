a776c5687448c7b6f5d569635245e5d5b2ae9105
Jay <mjzeel@gmail.com>
2017-08-19 13:35:16 +0530

    file indentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
+
 import { LoginVariable } from './global';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
-	
 })
 export class AppComponent {
 	
