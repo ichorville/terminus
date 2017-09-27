b4c56813e067975d82a261faad10ca5bb9fe4be4
Manoj Jayalath <manojj@axienta.lk>
2017-09-27 11:08:26 +0530

    base url changed to point maharaja UAT

--- src/app/global.ts
+++ src/app/global.ts
@@ -2,8 +2,13 @@ export const GlobalVariable = Object.freeze({
 	// BASE_API_URL: 'http://10.20.10.16/NestleAPI/api'
 	// http://10.20.10.16/VantageMerchandiser/api' //http://localhost:64598/api'
 
-	BASE_API_URL: 'http://www.axienta.lk/VantageMerchandiser45/api',
-	BASE_API_URL_2: 'http://www.axienta.lk/VantageDashboardAPI/api/home'
+	// exposed URL
+	//BASE_API_URL: 'http://www.axienta.lk/VantageMerchandiser45/api',
+
+	BASE_API_URL_2: 'http://www.axienta.lk/VantageDashboardAPI/api/home',
+
+	// MAHARAJA UAT
+	BASE_API_URL: 'http://203.189.74.54//Merchandiser/api'
 	
 });
 
